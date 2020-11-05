8777ed57bbd9e5849f0bea844a6c80adb892baf6
penny254 <ayebazibwepeninah19@gmail.com>
2020-11-05 17:43:52 -0500

    file ready to commit

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,41 +1,41 @@
-let rollDiceinput=document.querySelector("#DieRolls")
-let rollButton=document.querySelector("#roll-Button")
-let ShowAllRolls=document.querySelector("#Show-all-rolls")
-let rollResults=document.querySelector("#rollResults")
+let rollDiceinput=document.querySelector("#roll-Dice")
+let rollButton=document.querySelector("#roll-button")
+let ShowAllRolls=document.querySelector("#show-all-rolls")
+let rollResults=document.querySelector("#Results")
+let total=document.querySelector("#total")
 let allRolls=[]
 
-
-
-if(rollButton)
 rollButton.addEventListener("click",function(){
-    console.log(rollButton<=3)
-    let RandomNumber = (min, max) => {
-        return Math.floor(Math.random() * (min - max+ 1)) + min;
-        rollDice = (number) => (1, 6);
-      };
-
-    let maxRolls=number(rollDiceinput.value)
-    let allRoll=[]
+    console.log(rollButton)
+    let maxRolls=Number(rollDiceinput.value)
+    console.log(rollDiceinput)
+    let totaldice=0
     let count=0
     while(count<maxRolls){
-        allRolls.push("the number rolled"+count)
-        count++;
-    }
-    console.log(allRolls)
-})
-    if (ShowAllRolls)
-    ShowAllRolls.addEventListener("click",function(){
-        console.log(showAllRoll==7)
-        let count=0;
-        while(count < allRolls.length){
-            console.log(allRolls[count])
+  let  RandomNumber = Math.floor(Math.random()*6)+1
+          allRolls.push(RandomNumber)
+          totaldice+=RandomNumber
+          total.innerHTML=totaldice 
+        count++;}
     
-    let total=0
-    let rollResults=0
-     while(total>=allRolls){
-        rollResults.innerHTML+= "<li>"+rollResults+[count+4]+"</li>"}
-        count++;}    
-     })  
+})
+    
+    ShowAllRolls.addEventListener("click",function(){
+    let count=0;
+      while(count < allRolls.length){
+        
+        rollResults.innerHTML += "<li>"+ allRolls[count] +"</li>"
+       
+    count++;}})
+    
+
+    
+    
+    
+     
+             
+     
+       
 
     
   
